1fe2da2ffae28047ffae25615829d5d43e83584a
slf0Dev <73026438+slf0Dev@users.noreply.github.com>
2022-05-26 04:13:43 +0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -343,10 +343,10 @@ function UILibrary.Main(PrjName,HideKey)
 					TweenService:Create(Nofitication,TweenInfo.new(0.3),{BackgroundTransparency = 1}):Play()
 					TweenService:Create(v,TweenInfo.new(0.3),{BackgroundTransparency = 1}):Play()
 					TweenService:Create(v,TweenInfo.new(0.3),{TextTransparency = 1}):Play()
-					wait(0.5)
-					Nofitication:Destroy()
 				end)
 			end
+			wait(0.5)
+			Nofitication:Destroy()
 		end)
 		
 		NofiticationButton.MouseEnter:Connect(function()
